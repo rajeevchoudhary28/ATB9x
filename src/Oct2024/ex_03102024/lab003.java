524949f69130e39dbc6174a44e5a487b819f9c6c
Rajeev.Choudhary <rc.ece2016.2003@gmail.com>
2024-11-21 22:21:04 +0530

    04-10-2024

---
+++ src/Oct2024/ex_03102024/lab003.java
@@ -0,0 +1,10 @@
+package Oct2024.ex_03102024;
+
+public class lab003 {
+    public static void main(String[] args) {
+        System.out.println(10 + 2);  // add
+        System.out.println(30 - 10);  //sub
+        System.out.println(40 * 6);  //multiply
+        System.out.println(60 / 2);  //divide
+    }
+}
---
+++ src/Oct2024/ex_04102024/lab001.java
@@ -0,0 +1,7 @@
+package Oct2024.ex_04102024;
+
+public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
