7aab976586a728e84c0ba438c0b397cd11cd5a5d
Rajeev.Choudhary <rc.ece2016.2003@gmail.com>
2025-01-09 20:16:30 +0530

    07-10-2024 all files

--- src/Nov2024/ex_07112024_Inheritance/Multilevel/grandfather.java
+++ src/Nov2024/ex_07112024_Inheritance/Multilevel/grandfather.java
@@ -2,7 +2,9 @@ package Nov2024.ex_07112024_Inheritance.Multilevel;
 
 public class grandfather {
 
-
+    void home(){
+        System.out.println("4BHK");
+    }
     void gf(){
         System.out.println("GF");
     }
